c9f9492cf801f01e4f31e5b0a31caa5cf19d5d31
Aleksandr Vinokurov <98280490+VinokurovAleksandr@users.noreply.github.com>
2023-11-30 22:51:42 +0200

    add filter

--- cd/src/App.js
+++ cd/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { Counter } from './components/Counter';
 import ClassCounter from './components/ClassCounter';
 import moduleName from './components/PostItem'
@@ -6,49 +6,93 @@ import  PostItem  from './components/PostItem';
 import PostsList from './components/PostsList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
-import PostForm from './components/PostForm'
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect'
 import './styles/App.css'
 
 
 function App() {
+
   const [posts, setPosts] = useState([
     { id: 1, title: 'Javascript', body: 'Description' },
-    { id: 2, title: 'Javascript 2', body: 'Description' },
-    { id: 3, title: 'Javascript 3', body: 'Description' }
+    { id: 2, title: 'React', body: 'Fuu' },
+    { id: 3, title: 'HTML', body: 'Description' }
   ]);
+  const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('')
 
+
+  const options = [
+    { value: 'title', name: 'From title' },
+    { value: 'body', name: 'From text' }
+  ];
+
+
+  const sortedPosts = useMemo(() => {
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+    a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const soretedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  },[searchQuery, sortedPosts])
+   
   // const [title, setTitle] = useState('');
   // const [body, setBody] = useState('');
 
 
   // За допомогою хука useRef  ми можемо на пряму отримувати доступ до ДОМ елементу
   // const bodyInputRef = useRef();
+ 
+ 
+
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
-  }
+  };
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
-  }
+  };
 
+  const sortPost = (sort) => {
+    setSelectedSort(sort)
+  };
 
   return (
     
       <div className='App'>
       <PostForm create={createPost} />
-           {/* <Counter/> */}
-      {/* <ClassCounter/> */}
-      {/* <PostItem post={{id: 1, title:'Javascript', body: 'Description'} } /> */}
-      {posts.length !== 0 ? <PostsList
+      <hr style={{ margin: '15px 0' }} />
+      <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder={'Search...'}
+        />
+        <MySelect
+        value={selectedSort}
+        onChange={sortPost}
+        defaultValue='Sort'
+        options={options}
+      />
+      </div>
+
+      {soretedAndSearchedPosts.length !== 0 ? <PostsList
         remove={removePost}
-        posts={posts}
-        title={'Post about JS'} /> : <h2 style={{textAlign: 'center'}}>Dont posts</h2>
+        posts={soretedAndSearchedPosts}
+        title={'Post about JS'} />
+        :
+        <h2 style={{ textAlign: 'center' }}>
+          Dont posts
+        </h2>
    }
       
       </div>
-     
-    
   );
 }
 
